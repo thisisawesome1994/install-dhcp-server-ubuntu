725724950267de6ff3c257b846f7457bf5eaad92
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2022-08-30 17:18:44 +0200

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -9,4 +9,7 @@ iptables -A FORWARD -i $EXTIF -o $INTIF -m state --state ESTABLISHED,RELATED -j 
 iptables -A FORWARD -i $INTIF -o $EXTIF -j ACCEPT
 iptables -A FORWARD -j REJECT --reject-with icmp-host-prohibited
 iptables -t nat -A POSTROUTING -o $EXTIF -j MASQUERADE
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -i $EXTIF -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 # more rules if required
